cb3611c33c785c54b196154316a6d70dd46b3959
studenovAlex <alexstudenov44@gmail.com>
2021-12-26 16:38:20 +0200

    added .gitignore. added bot setup info in readme. added test answer

--- bot.php
+++ bot.php
@@ -1,1 +1,28 @@
 <?php
+
+#get and decode request
+$data = json_decode(file_get_contents('php://input'), true);
+
+#write data to log file
+file_put_contents('logs.txt', print_r($data, 1) . "\n", FILE_APPEND | LOCK_EX);
+
+#check user id
+if (empty($data['message']['chat']['id'])) {
+	exit();
+}
+
+#constants
+define('TOKEN', '');
+define('NGROK', '');
+
+#webhook
+$webHookUrl = 'https://api.telegram.org/bot' . TOKEN . '/setWebhook?url=' . NGROK;
+
+$message = $data['message']['text'];
+
+$params = [
+	'chat_id' => $data['message']['chat']['id'],
+	'text' => 'Hello World!'
+];
+
+file_get_contents('https://api.telegram.org/bot' . TOKEN . '/sendMessage?' . http_build_query($params));
