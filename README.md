66bd97a9b8010b2977565620711d78761bf93083
studenovAlex <alexstudenov44@gmail.com>
2021-12-26 23:13:26 +0200

    added text messages answers. readme changes

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # telegram-bot-php
 
-## cd to project folder
+- `cd` to project folder
 
-## php -S localhost:9090 or your port
+- `php -S localhost:9090` or your port
 
-## ngrok http 9090
+- `ngrok http 9090`
 
-## set webhook via curl "curl -d url=NGROK_LINK/bot.php -X POST https://api.telegram.org/botYOUR_TOKEN/setWebhook"
+- set webhook via curl `curl -d url=NGROK_LINK/bot.php -X POST https://api.telegram.org/botYOUR_TOKEN/setWebhook`
 
-## or link in browser "https://api.telegram.org/botYOUR_TOKEN/setWebhook?url=NGROK_LINK/bot.php"
+- or link in browser `https://api.telegram.org/botYOUR_TOKEN/setWebhook?url=NGROK_LINK/bot.php`
--- bot.php
+++ bot.php
@@ -18,11 +18,66 @@ define('NGROK', '');
 #webhook
 $webHookUrl = 'https://api.telegram.org/bot' . TOKEN . '/setWebhook?url=' . NGROK;
 
-$message = $data['message']['text'];
+#send answer within method and data
+function sendTelegram($method, $response)
+{
 
-$params = [
-	'chat_id' => $data['message']['chat']['id'],
-	'text' => 'Hello World!'
-];
+	#create a new cURL resource
+	$ch = curl_init('https://api.telegram.org/bot' . TOKEN . '/' . $method);  
 
-file_get_contents('https://api.telegram.org/bot' . TOKEN . '/sendMessage?' . http_build_query($params));
+	#set other appropriate options
+	curl_setopt($ch, CURLOPT_POST, 1);  
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $response);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+
+	#grab URL and pass it
+	$res = curl_exec($ch);
+
+	#close cURL resource, and free up system resources
+	curl_close($ch);
+ 
+	return $res;
+}
+
+#check is empty message
+if (!empty($data['message']['text'])) {
+
+	$text = $data['message']['text'];
+
+	switch ($text) {
+
+		case 'hello':
+			$method = 'sendMessage';
+			$sendData = [
+				'text' => 'Hi, ' . $data['message']['chat']['first_name'] . '!'
+						];
+			break;
+
+		case 'photo':
+			$method = 'sendPhoto';
+			$sendData = [
+				'photo' => curl_file_create(__DIR__ . '/images/' . 'cat.jpg')
+						];
+			break;
+
+		case 'file':
+			$method = 'sendDocument';
+			$sendData = [
+				'document' => curl_file_create(__DIR__ . '/files/' . 'example.txt')
+						];
+			break;
+
+		default:
+			$method = 'sendMessage';
+			$sendData = [
+				'text' => 'Repeat, please'
+						];
+			break;
+	}
+
+	$sendData['chat_id'] = $data['message']['chat']['id'];
+
+	sendTelegram($method, $sendData);
+
+}
